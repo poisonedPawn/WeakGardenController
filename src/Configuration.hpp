4297f0aafacecaae92bd3ced985127a2b1f843a1
poisonedPawn <joc.thranduil@gmail.com>
2021-05-17 01:55:21 +0200

    Conf for git

--- src/Configuration.hpp
+++ src/Configuration.hpp
@@ -2,17 +2,17 @@
 #define LED D3
 #define MOISTURES A0
 
-#define WIFI_SSID "poisonedNetwork"
-#define WIFI_PASSWORD "72M431cQb"
+#define WIFI_SSID "WIFI SSID"
+#define WIFI_PASSWORD "PASS"
 
-#define MQTT_SERVER "192.168.1.49"
+#define MQTT_SERVER "MQTT SERVER IP"
 #define MQTT_PORT 1883
 #define MQTT_USER "YOUR_MQTT_USER"
 #define MQTT_PASSWORD "YOUR_MQTT_PASS"
 
 #define BUFFER_SIZE 50
 
-#define MQTT_TOPICHS "/casa/jardin/hum_suelo"
-#define MQTT_TOPICH "/casa/jardin/humedad"
-#define MQTT_TOPICT "/casa/jardin/temperatura"
-#define MQTT_TOPICP "/casa/jardin/presion"
+#define MQTT_TOPICHS "topic1"
+#define MQTT_TOPICH "topic2"
+#define MQTT_TOPICT "topic3"
+#define MQTT_TOPICP "topic4"
